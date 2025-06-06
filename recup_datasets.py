ce3c9a38142c5ceeecec40cb8b5c5e17d7b19b6f
sophieperrinlyon2 <sophie.perrin1@univ-lyon2.fr>
2025-06-06 12:37:57 +0000

    creation du programme recup_datasets.py pour récupérer les datasets weibo et reddit de GADBench

---
+++ recup_datasets.py
@@ -0,0 +1,40 @@
+import argparse
+import time
+import utils as ut
+from utils import *
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings("ignore")
+seed_list = list(range(3407, 10000, 10))
+
+def set_seed(seed=3407):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--datasets', type=str, default=None)
+args = parser.parse_args()
+
+
+datasets = ['reddit', 'weibo']
+
+
+if args.datasets is not None:
+    if '-' in args.datasets:
+        st, ed = args.datasets.split('-')
+        datasets = datasets[int(st):int(ed)+1]
+    else:
+        datasets = [datasets[int(t)] for t in args.datasets.split(',')]
+    print('Evaluated Datasets: ', datasets)
+
+
+for dataset_name in datasets:
+    data = ut.Dataset(dataset_name)
